66ad016b50fd04985f7172be13896fe710ad6afb
Naej <jean0poree@gmail.com>
2017-05-09 14:36:17 +0200

    new file agencement really

---
+++ src/sample/Main.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private FXMLLoader loader;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("sample.fxml"));
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+
+        Parent root = (Parent) loader.load(getClass().getResource("sample.fxml").openStream());
+
+        primaryStage.setTitle("Hello World");
+
+        primaryStage.setScene(new Scene(root, 600, 400));
+
+
+
+        primaryStage.show();
+
+        ((MainGameController) loader.getController()).start();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
