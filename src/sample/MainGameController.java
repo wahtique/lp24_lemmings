aef690dfc6de46d56116c8df4a384a20aea4ca4f
naej <jean0poree@gmail.com>
2017-05-10 01:27:49 +0200

    found usefull relationship

--- src/sample/MainGameController.java
+++ src/sample/MainGameController.java
@@ -33,7 +33,7 @@ public class MainGameController {
 
         //  System.out.println(loadedImg.getPixelReader().getColor(0,0));
         System.out.println(loadedImg.getWidth());
-        centeredDrawImage(new ImageView(modifiable),200,200,gc);
+        notCenteredDrawImage(new ImageView(modifiable),10,10,gc);
 
     }
 
@@ -44,10 +44,17 @@ public class MainGameController {
     public void onMouseClick (MouseEvent e){
         PixelWriter writer = modifiable.getPixelWriter();
 
-       // TODO: find relationship between lowerbound and upperbound of the image and heigth & weigth of the image
-        writer.setColor((int)(e.getX() -200),(int)(e.getY()-200), Color.color(1, 0.0078, 0));
-        centeredDrawImage(new ImageView(modifiable),200,200,gc);
+
+        // writer.setColor((int)(e.getX() -200),(int)(e.getY()-200), Color.color(1, 0.0078, 0));
+        //writer.setColor((int)(loadedImg.getWidth())-1,(int)(loadedImg.getHeight())-1, Color.color(1, 0.0078, 0));
+        System.out.println((int)(e.getX())-1);
+        //FORMULA: (int)(mouse.getX()-imagepositionX)-1 (all coordinates are canvas relative)
+        writer.setColor((int)(e.getX()-30)-1,(int)(e.getY()-30)-1, Color.color(1, 0.0078, 0));
+
+        notCenteredDrawImage(new ImageView(modifiable),30,30,gc);
         //System.out.println("Clic at "+ e.getX() + " : " + e.getY());
+        System.out.println("Width: "+ loadedImg.getWidth() + " Heigth : " + loadedImg.getHeight());
+    //    System.out.println(""+ loadedImg.getPixelReader(). + " : " + e.getY());
 
         //this is how you can print an image on the canvas
 
@@ -62,6 +69,11 @@ public class MainGameController {
     public void centeredDrawImage(ImageView img, double x, double y, GraphicsContext gc){
         img.setX(x-img.getImage().getWidth()/2);
         img.setY(y-img.getImage().getHeight()/2);
+        gc.drawImage(img.getImage(),img.getX(),img.getY());
+    }
+    public void notCenteredDrawImage(ImageView img, double x, double y, GraphicsContext gc){
+        img.setX(x);
+        img.setY(y);
         gc.drawImage(img.getImage(),img.getX(),img.getY());
     }
 }
