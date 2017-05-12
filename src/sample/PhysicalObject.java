df31e3e5ec13520783d34a84470ad8eecb5d945e
Ame <lechevantonyann@gmail.com>
2017-05-12 19:44:01 +0200

    Physical Object and first Lemmings

---
+++ src/sample/PhysicalObject.java
@@ -0,0 +1,60 @@
+package sample;
+
+
+/**
+ * Created by yann on 12/05/17.
+ */
+public class PhysicalObject {
+    Vector position;
+    Vector speed;
+    Vector forces;
+
+    public PhysicalObject(Vector position, Vector speed, Vector forces) {
+        this.position = position;
+        this.speed = speed;
+        this.forces = forces;
+    }
+
+    public PhysicalObject() {
+        this.position = new Vector(0,0);
+        this.speed = new Vector(0, 0);
+        this.forces = new Vector(0, 0);
+    }
+
+    public Vector getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector position) {
+        this.position = position;
+    }
+
+    public Vector getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Vector speed) {
+        this.speed = speed;
+    }
+
+    public Vector getForces() {
+        return forces;
+    }
+
+    public void setForces(Vector forces) {
+        this.forces = forces;
+    }
+
+    public void addForces(Vector f){
+        this.forces.add(f);
+    }
+
+    public void addSpeed(Vector s){
+        this.speed.add(s);
+    }
+
+    public void update(double deltaTime){
+        this.speed = speed.add(this.forces.mulScal(deltaTime));
+        this.position = position.add(this.speed.mulScal(deltaTime));
+    }
+}
