991def36745709fc46a7596a1bb152cae775e68a
William <wil-94@hotmail.fr>
2017-05-20 18:22:57 +0200

    simple commit before merging. nothing new

---
+++ src/sample/MainMenuController.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+/**
+ * @author William
+ * @since 15/05/2017
+ */
+public class MainMenuController
+{
+
+
+    public void sayHelloWorld(ActionEvent actionEvent)
+    {
+
+    }
+}
