49ecf15a16d6ddf946bf0b31c28c277743430fc5
William <wil-94@hotmail.fr>
2017-05-21 14:38:42 +0200

    Menus de navigation OK. Je planche encore sur lobtention des listes de sauvegarde. Et cest toujours moche. Mais bon. Et ya une partie du code vraiment plus degueue que le reste.

--- src/sample/MainMenuController.java
+++ src/sample/MainMenuController.java
@@ -1,6 +1,16 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * @author William
@@ -8,10 +18,89 @@ import javafx.event.ActionEvent;
  */
 public class MainMenuController
 {
+    @FXML
+    private Button newGameButton;
+    @FXML
+    private Button loadGameButton;
+    @FXML
+    private Button settingsButton;
+    @FXML
+    private Button creditsButton;
+    @FXML
+    private Button quitButton;
+    @FXML
+    private Button newGameBackButton;
+    @FXML
+    private Button loadGameLoadButton;
+    @FXML
+    private Button loadGameBackButton;
+    @FXML
+    private Button settingsBackButton;
+    @FXML
+    private Button creditsScreenBackButton;
 
 
-    public void sayHelloWorld(ActionEvent actionEvent)
+    public MainMenuController()
     {
+    }
+
+    @FXML
+    private void initialze(){}
+
+    /**
+     * Method detecting which button activated and calling switchToScene with the right parameter
+     * @param event event triggering the method
+    * */
+    @FXML
+    private void switchSceneOnButtonAction(ActionEvent event) throws IOException
+    {
+        //we start by selecting detecting which FXML to use
+        Button source = (Button) event.getSource();
+        if (source == newGameButton )
+        {
+            switchToScene("newGameMenu", (Stage)source.getScene().getWindow());
+        }
+        else if (source == loadGameButton)
+        {
+            switchToScene("loadGameMenu",(Stage)source.getScene().getWindow());
+        }
+        else if(source == settingsButton)
+        {
+            switchToScene("settingsMenu",(Stage)source.getScene().getWindow());
+        }
+        else if(source == creditsButton)
+        {
+            switchToScene("creditsScreen",(Stage)source.getScene().getWindow());
+        }
+        else if(source == quitButton)
+        {
+            Stage stage = (Stage) quitButton.getScene().getWindow();
+            stage.close();
+        }
+        else
+        {
+            //should be only the diverse back to main menu button handled in this last case
+            switchToScene("MainMenu",(Stage)source.getScene().getWindow());
+        }
 
     }
+
+
+    /**
+     * Method switching to another scene in the same stage
+     * @paranm sceneName String corresponding to the name of the FXML, without file extension
+     * @param stage the stage on which we operate. Gotta find a better way to access it.
+    */
+    @FXML
+    private void switchToScene(String sceneName, Stage stage) throws IOException
+    {
+        FXMLLoader loader = new FXMLLoader();
+        FileInputStream fin = new FileInputStream("src/sample/" + sceneName +".fxml");
+        AnchorPane root = (AnchorPane) loader.load(fin);
+        Scene sc = new Scene(root);
+        stage.setScene(sc);
+        stage.show();
+        fin.close();
+    }
+
 }
